0020b8ef1feede0053b7bee113142ee14ca79e5c
Jerry Gu <ncq9fn@virginia.edu>
2023-09-02 15:19:57 -0400

    beauty

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
 import keyboard
 import pygame
 
+FRENCHY = False
+
 pygame.mixer.init()
+pygame.mixer.set_num_channels(100)
 
-RammusOK = "Rammus OK..wav"
-OOF = "3"
+q_audio = "Rammus OK..wav"
+w_audio = "Mhm.ogg"
+e_audio = "Yep.ogg"
+r_audio = "Alright.ogg"
+f_audio = "what the fuck.ogg"
+d_audio = "HM.ogg"
 
 
-def play_audio():
-    audio_file = RammusOK
+def play_audio(audio_file):
     pygame.mixer.music.load(audio_file)
     pygame.mixer.music.play()
 
 
-keyboard.add_hotkey('q', lambda : play_audio())
+def FRENCH():
+    global q_audio, w_audio, e_audio, r_audio
+    global FRENCHY
+    play_audio(f_audio)
+    if FRENCHY:
+        FRENCHY = False
+        q_audio = "Rammus OK..wav"
+        w_audio = "Mhm.ogg"
+        e_audio = "Yep.ogg"
+        r_audio = "Alright.ogg"
+    else:
+        FRENCHY = True
+        q_audio = "OUIIIIIIII.ogg"
+        w_audio = "SAAAAAAAA UUUUUURGH.ogg"
+        e_audio = "OOOOOOOOHHHHHHHHHHH.ogg"
+        r_audio = "HMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.ogg"
+
+
+keyboard.add_hotkey('q', lambda : play_audio(q_audio))
+keyboard.add_hotkey('w', lambda : play_audio(w_audio))
+keyboard.add_hotkey('e', lambda : play_audio(e_audio))
+keyboard.add_hotkey('r', lambda : play_audio(r_audio))
+
+keyboard.add_hotkey('f', lambda : FRENCH())
+keyboard.add_hotkey('d', lambda : play_audio(d_audio))
+
+keyboard.add_hotkey('tab', lambda : play_audio())
+
 keyboard.wait()
