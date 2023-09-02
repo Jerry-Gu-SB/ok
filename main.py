ebba1a595ea9da9b48a3978a82b4487bba39d4cf
Jerry Gu <ncq9fn@virginia.edu>
2023-09-02 15:33:35 -0400

    finishing touches

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ e_audio = "Yep.ogg"
 r_audio = "Alright.ogg"
 f_audio = "what the fuck.ogg"
 d_audio = "HM.ogg"
-
+tab_audio = "Right.ogg"
 
 def play_audio(audio_file):
     pygame.mixer.music.load(audio_file)
@@ -20,7 +20,7 @@ def play_audio(audio_file):
 
 
 def FRENCH():
-    global q_audio, w_audio, e_audio, r_audio
+    global q_audio, w_audio, e_audio, r_audio, d_audio, tab_audio
     global FRENCHY
     play_audio(f_audio)
     if FRENCHY:
@@ -30,13 +30,15 @@ def FRENCH():
         e_audio = "Yep.ogg"
         r_audio = "Alright.ogg"
         d_audio = "HM.ogg"
+        tab_audio = "Right.ogg"
     else:
         FRENCHY = True
         q_audio = "OUIIIIIIII.ogg"
         w_audio = "SAAAAAAAA UUUUUURGH.ogg"
         e_audio = "OOOOOOOOHHHHHHHHHHH.ogg"
         r_audio = "HMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.ogg"
-        d_audio = ""
+        d_audio = "WOOA PUP.ogg"
+        tab_audio = "EHHHHH.ogg"
 
 
 keyboard.add_hotkey('q', lambda : play_audio(q_audio))
@@ -47,6 +49,6 @@ keyboard.add_hotkey('r', lambda : play_audio(r_audio))
 keyboard.add_hotkey('f', lambda : FRENCH())
 keyboard.add_hotkey('d', lambda : play_audio(d_audio))
 
-keyboard.add_hotkey('tab', lambda : play_audio())
+keyboard.add_hotkey('tab', lambda : play_audio(tab_audio))
 
 keyboard.wait()
